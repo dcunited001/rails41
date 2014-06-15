2b94c7681a0e2b729d1995e2efea072f39db3c56
David Conner <dconner.pro@gmail.com>
2014-06-15 17:47:41 -0400

    * added Google Analytics to template
    * added integration environment
    
    * updated gitignore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def has_ganalytics?
+    Rails.application.secrets.ganalytics
+  end
+
+  def ganalytics_config
+    { tracking_id: Rails.application.secrets.ganalytics['tracking_id'], domain_name: Rails.application.secrets.ganalytics['domain_name'] }
+  end
 end

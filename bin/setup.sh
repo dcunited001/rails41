14445ca1b3118b4f5b37efb6b59684fb98decc1e
David Conner <dconner.pro@gmail.com>
2014-08-26 04:10:52 -0400

    * added & updated the bin/start.sh script based on pmx-rails
    * moved some of the start.sh content to setup.sh
    
    * this way the final command can be overridden

---
+++ bin/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bundle config build.pg --with-pg-config=/usr/bin/pg_config
+bundle install --gemfile=/home/app/webapp/firstapp/Gemfile
+bundle exec rake db:create
+bundle exec rake db:migrate
